2e1ea3e02515c303eb874d7993de153123a35624
Manuel Guarniz <meguarnizc@indracompany.com>
2020-07-03 00:37:57 -0500

    config vscode

--- app.js
+++ app.js
@@ -30,3 +30,35 @@ decrement.addEventListener('click', (e) => {
 })
 
 window.addEventListener('resize', draw);
+
+
+// {
+//   "editor.tabSize": 2,
+//   "window.zoomLevel": -1,
+//   "git.autofetch": true,
+//   "editor.suggestSelection": "first",
+//   "vsintellicode.modify.editor.suggestSelection": "automaticallyOverrodeDefaultValue",
+//   "files.exclude": {
+//       "**/.classpath": true,
+//       "**/.project": true,
+//       "**/.settings": true,
+//       "**/.factorypath": true
+//   },
+//   "python.jediEnabled": false,
+//   "php.validate.executablePath": "D:\\xampp\\php\\php.exe",
+//   "php.executablePath": "D:\\xampp\\php\\php.exe",
+//   "php.suggest.basic": false,
+//   "diffEditor.ignoreTrimWhitespace": true,
+//   "plsql-language.oracleConnection.enable": true,
+//   "editor.renameOnType": true,
+//   "cSpell.language": "en",
+//   "workbench.iconTheme": "material-icon-theme",
+//   "terminal.integrated.shell.windows": "C:\\WINDOWS\\System32\\WindowsPowerShell\\v1.0\\powershell.exe",
+//   "java.jdt.ls.vmargs": "-XX:+UseParallelGC -XX:GCTimeRatio=4 -XX:AdaptiveSizePolicyWeight=90 -Dsun.zip.disableMemoryMapping=true -Xmx1G -Xms100m -javaagent:\"c:\\Users\\Indra\\.vscode\\extensions\\gabrielbb.vscode-lombok-1.0.1\\server\\lombok.jar\"",
+//   // "editor.fontFamily": "Fira Code",
+//   "editor.fontFamily": "CascadiaCode Nerd Font, Consolas, 'Courier New', monospace",
+//   "editor.fontLigatures": true,
+//   "java.semanticHighlighting.enabled": true,
+//   "editor.fontSize": 15,
+//   "python.languageServer": "Microsoft"
+// }
